201458fcb9b2e4b2821a2254f6df35d3d0ef8089
gookie-dev <36005381+gookie-dev@users.noreply.github.com>
2022-03-07 00:43:59 +0100

    v1.0

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "cant find token on given download id"
+
+@app.route('/<id>')
+def id(id):
+    try:
+        r = requests.get(url = "https://bowfile.com/" + id, headers = {"Cookie": "filehosting=20nupilfrt7df5b8hurj69prio"})
+        token = r.text.split("showFileInformation(")[1].split(");")[0]
+    except:
+        return "cant find token on given download id"
+    ddl = requests.request('HEAD', url = "https://bowfile.com/account/direct_download/" + token, headers = {"Cookie": "filehosting=20nupilfrt7df5b8hurj69prio"}) 
+    return ddl.url
